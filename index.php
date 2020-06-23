24374bf1a8941bab8b08d723e09f3aed526b30df
valou423 <valentin.chevaillier@free.fr>
2020-06-23 14:41:41 +0200

    correction bug de connexion

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
+<!DOCTYPE html>
 <?php
 session_start(); //demarage de la session
 
 $id_session = session_id(); //Chaque utilisateur a une ID de session a chaque demarage du site
 ?>
 
-<!DOCTYPE html>
+
 <html>
     <head>
         <meta name="author" content="Valentin CHEVAILLIER">
@@ -13,13 +14,16 @@ $id_session = session_id(); //Chaque utilisateur a une ID de session a chaque de
     </head>
     <body>
         <h1 class="titre_page">Serveur de la famille</h1>
+        
         <?php
-        if ($_SESSION['etat']=='connect') 
-                echo '<h4>Vous êtes connecté en tant que '.$_SESSION['login'].'</h4>';
+        if ($_SESSION['etat']=='connect') {
+            echo '<h4>Vous êtes connecté en tant que '.$_SESSION['login'].'</h4>';
+    
+        }
             
-        if ($_SESSION['etat']=='disconnect' or $_SESSION['etat']=='') 
+        if ($_SESSION['etat']=='disconnect' or $_SESSION['etat']=='') {
                 include('Connexion.php');
-            
+        }
 
         ?>
         <p>La famille CHEVAILLIER est une grande famille. Elle est composée de 
@@ -27,7 +31,7 @@ $id_session = session_id(); //Chaque utilisateur a une ID de session a chaque de
             Chaqun des membres est decrit dans la liste ci-dessous.</p>
 
         <?php
-        include("/Ressource/connexion/connexion.php") #onglet de connextion
+        include("Ressource/connexion/Connexion.php"); #onglet de connextion
         include("Menu2.php");
         include("Ressource/inscription/inscription.php");
         ?>
